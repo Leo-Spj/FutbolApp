aa6401c982a453805c10a69d713dda864934f7ff
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-04-12 09:58:22 -0500

    Actualizo repositorio PosicionRepository para utilizar ListCrudRepository en lugar de JpaRepository

--- src/main/java/com/civa/futbolapp/persistence/repository/PosicionRepository.java
+++ src/main/java/com/civa/futbolapp/persistence/repository/PosicionRepository.java
@@ -1,7 +1,7 @@
 package com.civa.futbolapp.persistence.repository;
 
 import com.civa.futbolapp.persistence.entity.PosicionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
-public interface PosicionRepository extends JpaRepository<PosicionEntity, Integer> {
+public interface PosicionRepository extends ListCrudRepository<PosicionEntity, Integer> {
 }
