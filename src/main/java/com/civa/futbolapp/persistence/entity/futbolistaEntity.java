dd84ffd00bb3f3383c5ecd96b229550efc662d09
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-04-12 09:57:49 -0500

    Actualizo entidad FutbolistaEntity y añadir entidad PosicionEntity

--- src/main/java/com/civa/futbolapp/persistence/entity/futbolistaEntity.java
+++ src/main/java/com/civa/futbolapp/persistence/entity/futbolistaEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.time.LocalDate;
 
@@ -16,19 +18,23 @@ public class FutbolistaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "futbolistaid")
+    @Column(name = "futbolista_id")
     private int id;
 
+    @Column(nullable = false, length = 50)
     private String nombre;
 
+    @Column(nullable = false, length = 50)
     private String apellido;
 
-    @Column(columnDefinition = "DATE")
+    @Column(name = "fecha_nacimiento", nullable = false, columnDefinition = "DATE")
     private LocalDate fechaNacimiento;
 
     private String caracteristicas;
 
-    @OneToOne
-    @JoinColumn(name = "posicionid", referencedColumnName = "posicionid")
+    //para posicion
+    @ManyToOne
+    @JoinColumn(name = "posicion_id", referencedColumnName = "posicion_id")
+    @Fetch(FetchMode.JOIN)
     private PosicionEntity posicion;
 }
