a2965d82f6ab5d9d8f162cd5d7a463881c47626f
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-04-13 14:36:44 -0500

    método getAll en FutbolistaService.java para ordenar por ID descendente

--- src/main/java/com/civa/futbolapp/service/FutbolistaService.java
+++ src/main/java/com/civa/futbolapp/service/FutbolistaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,8 +30,8 @@ public class FutbolistaService{
     }
 
     public Page<FutbolistaEntity> getAll(int page, int size){
-        Pageable pageable = PageRequest.of(page, size);
-        return futbolistaRepository.findAll(pageable);
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by("id").descending());
+        return futbolistaRepository.findAll(pageRequest);
     }
 
 
